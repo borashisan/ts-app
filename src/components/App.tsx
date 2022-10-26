c069d5ab8f7894af6935801053cfd11d6b92d971
borashisan <tomochuro731@gmail.com>
2022-10-27 08:52:05 +0900

    -

---
+++ src/components/App.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, useState, FC, useCallback } from "react";
+import styled from "styled-components";
+import { MemoList } from "./MemoList";
+
+const App : FC = () => {
+  const [text, setText] = useState<string>("");
+
+  const [memos, setMemos] = useState<string[]>([])
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+setText(e.target.value);
+
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    setText("");
+  };
+
+  const onClickDelete = useCallback((index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  }, [memos])
+
+  return (
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <MemoList memos={memos} onClickDelete={onClickDelete}/>
+    </div>
+  );
+};
+
+const SButton = styled.button`
+  margin-left: 16px;
+`
+
+export default App
